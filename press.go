cc49818a090c4a61f3f2adf0cefac1a2f630bdd4
Ben Hancock <ben@benghancock.com>
2023-04-19 20:43:51 -0700

    implement basic templating logic

--- press.go
+++ press.go
@@ -1,10 +1,24 @@
-// press is a utility for applying HTML templates to content
+// press is a utility for applying simple HTML templates to content
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, press world!")
+	type pageData struct {
+		Title   string
+		Content template.HTML
+	}
+	data := pageData{"Hello World!", template.HTML("<p>This is some content</p>")}
+	t, err := template.ParseFiles("sample/template1.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = t.Execute(os.Stdout, data)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
