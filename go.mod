0509cf0f063d84e6eed18abad57ff3f29bfc847b
Ben Hancock <ben@benghancock.com>
2023-04-21 06:11:56 -0700

    implement parseTitle function

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/benghancock/press
 
 go 1.18
+
+require golang.org/x/net v0.9.0 // indirect
--- press.go
+++ press.go
@@ -3,9 +3,11 @@ package main
 
 import (
 	"flag"
+	"golang.org/x/net/html"
 	"html/template"
 	"log"
 	"os"
+	"strings"
 )
 
 var tf string
@@ -32,15 +34,42 @@ func main() {
 	}
 	// Because the user is providing this content, we assume it is safe
 	htmlContent := template.HTML(string(content))
+	pageTitle := parseTitle(string(content))
 	t, err := template.ParseFiles(tf)
 	if err != nil {
 		log.Fatal(err)
 	}
-	data := pageData{"Hello World!", htmlContent}
+	data := pageData{pageTitle, htmlContent}
 	err = t.Execute(os.Stdout, data)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-// TODO Implement function for retrieving title from content h1 tag
+// parseTitle retrieves the string in the first h1 tag
+func parseTitle(content string) string {
+	var title string
+	reader := strings.NewReader(content)
+	tokenizer := html.NewTokenizer(reader)
+
+	for {
+		token := tokenizer.Next()
+
+		switch token {
+
+		case html.ErrorToken:
+			// we've reached the end
+			return title
+
+		case html.StartTagToken, html.SelfClosingTagToken:
+			tagName, _ := tokenizer.TagName()
+			if string(tagName) == "h1" {
+				token = tokenizer.Next()
+				title = tokenizer.Token().String()
+				break
+			}
+		}
+	}
+
+	return title
+}
